0a13c91afa66037eb7872bd2566baca1d4a2c017
Siran Cao <siranchao@gmail.com>
2023-11-14 01:01:50 -0500

    add lazy loading and blur effect

--- components/DepartmentCard.js
+++ components/DepartmentCard.js
@@ -1,23 +1,47 @@
 import Card from 'react-bootstrap/Card';
 import styles from '@/styles/departments.module.css'
 import { useRouter } from 'next/router'
+import { useEffect, useRef } from 'react'
 
 export default function DepartmentCard({ department, deptId }) {
     const router = useRouter()
     const clickDept = () => {
-        // router.push(`artwork?departmentIds=${deptId}?all`)
         router.push(`department?departmentIds=${deptId}`)
     }
+
+    const imgRef = useRef(null)
+
+    function loaded() {
+        imgRef.current.classList.add('deptImgLoaded')
+    }
+
+    useEffect(() => {
+        const node = imgRef.current
+        if (node.complete) {
+            loaded()
+        }
+        else {
+            node.addEventListener('load', loaded)
+        }
+
+        return () => {
+            node.removeEventListener('load', loaded)
+        }
+    }, [])
 
     return (
         <>
             <Card className={styles.deptCard} onClick={clickDept}>
-                <Card.Img variant="top"
-                    src={`https://res.cloudinary.com/siran-chao/image/upload/v1683777371/museum_departments/met_dept_id_${deptId}.jpg`}
-                    alt='department-photo'
-                    round='false'
-                    className={styles.deptImg}
-                />
+                <div className="blurImg" style={{ backgroundImage: `url(/img/small-img/${deptId}.jpg)` }} >
+                    <Card.Img variant="top"
+                        src={`https://res.cloudinary.com/siran-chao/image/upload/v1683777371/museum_departments/met_dept_id_${deptId}.jpg`}
+                        alt='department-photo'
+                        round='false'
+                        className="deptImg"
+                        loading="lazy"
+                        ref={imgRef}
+                    />
+                </div>
                 <Card.Text className={styles.deptName}>{department}</Card.Text>
             </Card>
         </>
